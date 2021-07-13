6ea3666609a40e94ee760fc67c04fafe10b4826c
Lily <935673287@qq.com>
2021-07-13 12:18:59 +0800

    first commit

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,102 @@
+public class ArrayDeque<G> {
+    int size;
+    G[] array;
+    int front;
+    int rear;
+
+    /*Invariants:
+    1.the size is must be determined.
+    2.the first item's index is always (front+1)%8.Front always points to the front of the current's first item.
+    3.the current last item's index is always (rear)%8
+     */
+    public ArrayDeque() {
+        size = 0;
+        array = (G[]) new Object[8];
+        front = -1;
+        rear = -1;
+    }
+
+    public void addFirst(G item) {
+        if (front == -1 && array[7] == null) {
+            array[(front + 8) % 8] = item;
+            front = (front + 8) % 8 - 1;
+            size++;
+        } else {
+            int index = (front) % 8; //the next item's index
+            array[front] = item;
+            front--;
+            size++;
+        }
+    }
+
+    public void addLast(G item) {
+        int index = (rear + 1) % 8;//the next last item's index
+        rear = index;
+        if (array[index] == null) {
+            array[index] = item;
+        }
+        if (rear == front && size == 8) {
+            System.out.print("队列满了");
+        }
+        size++;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) return true;
+        else return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        int i = 0;
+        while (i < size) {
+            int index = (front + i + 1) % size;
+            System.out.print(array[index]);
+            System.out.print(" ");
+            i++;
+        }
+    }
+
+    public G removeFirst() {
+        G removed = array[(front + 1) % 8];
+        if (removed == null) {
+            return null;
+        } else {
+            array[(front + 1) % 8] = null;
+            front = (front + 1) % 8;
+        }
+        size--;
+        return removed;
+    }
+
+    public G removeLast() {
+        G removed = array[rear];
+        if (removed == null) {
+            return null;
+        } else {
+            array[rear] = null;
+            rear = (rear - 1) % 8;
+        }
+        size--;
+        return removed;
+    }
+
+    public G get(int index) {
+        return array[index];
+    }
+
+/*test
+    public static void main(String[] args) {
+        ArrayDeque<String> a1 = new ArrayDeque<>();
+        a1.addFirst("first added");
+        a1.addLast("second added");
+        a1.addFirst("third added");
+        a1.addFirst("kkkkkk");
+        a1.addLast("hhh");
+        a1.removeFirst();
+        a1.removeLast();
+    }*/
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,108 @@
+public class LinkedListDeque<T> {
+    private int size = 0;
+    TNode sentinal;
+    TNode tail;
+
+    private class TNode {
+        TNode pre;
+        TNode next;
+        T item;
+        public TNode(TNode p, T x,TNode n){
+            pre = p;
+            next = n;
+            item = x;
+        }
+    }
+
+    public LinkedListDeque(T x){
+        TNode t = new TNode(null, x,null);
+        sentinal = new TNode(null, null, t);
+        tail = new TNode(t, null,null);
+        if (t.pre == null) System.out.println("我想不通");
+        t.pre = sentinal;//为什么对了？？？t.pre难道不是null吗？？？
+        t.next = tail;
+        size = 1;
+    }
+    public LinkedListDeque(){
+        sentinal = new TNode(null, null, null);
+        tail = new TNode(sentinal, null,null);
+        sentinal = new TNode(null, null, tail);
+        tail.pre = sentinal;
+    }
+
+
+    public void addFirst(T item){
+        TNode f = new TNode(sentinal, item, sentinal.next);
+        f.next.pre = f;
+        sentinal.next = f;
+        size += 1;
+    }
+
+    public void addLast(T item){
+        TNode l = new TNode(tail.pre, item, tail);
+        tail.pre = l;
+        l.pre.next = l;
+        size += 1;
+    }
+
+    public boolean isEmpty(){
+        if (size == 0) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public void printDeque(){
+        TNode p = sentinal;
+        while(p != tail){
+            System.out.println(p.item);
+            p = p.next;
+        }
+    }
+
+
+    public T removeFirst(){
+        T first = sentinal.next.item;
+        sentinal.next = sentinal.next.next;
+        sentinal.next.pre = sentinal;
+        size--;
+        return first;
+    }
+
+    public T removeLast(){
+        T last = tail.pre.item;
+        tail.pre.pre.next = tail;
+        tail.pre = tail.pre.pre;
+        size--;
+        return last;
+    }
+
+    public T get(int index){
+        int i = 0;
+        TNode p = sentinal;
+        while(i <= index){
+            p = p.next;
+            i++;
+        }
+        return p.item;
+    }
+
+    public T getRecursive(int index){
+        return helper(sentinal.next, index);
+    }
+    private T helper(TNode s, int index){
+        if (index == 0){
+            return s.item;
+        }
+        else{
+            return helper(s.next, index - 1);
+        }
+    }
+
+}
